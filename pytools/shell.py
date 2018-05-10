907228495cf5ec7bd02edad35e7ae67a05fd9bad
Eric Ollivier <er.ollivier@gmail.com>
2018-05-10 16:51:43 +0200

    - Create the 'exploretools' module and the 'summary' function - Create the 'shell' module and the 'head' function

---
+++ pytools/shell.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""This module provides a several function useful in shell"""
+import sys
+import os
+
+def head(file='', lines=10, output=sys.stdout):
+    """
+    Print in the stdin the <lines> first lines of file <file>
+    """
+
+    try:
+        with open(file,'r') as fd:
+            for i in range(lines):
+                print(fd.readline(), end='', file=output)
+    except EOFError:
+        print('<End of file>')
+        
+    except Exception as err:
+        print(err, file=sys.stderr)
+        
