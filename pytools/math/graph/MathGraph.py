6aba8c2d3811cef63048142aa144317fe415b7d3
Eric Ollivier <er.ollivier@gmail.com>
2019-01-05 12:50:52 +0100

    Ajout d'un n° de version L'init inclus la création d'un objet Axes avec possibilité de définir la taille de la figure Ajout des fonctions - vecteur - fonction

--- pytools/math/graph/MathGraph.py
+++ pytools/math/graph/MathGraph.py
@@ -2,14 +2,17 @@
 """
 Created on Mon Dec 31 12:08:14 2018
 
-@author: Eric
+@author: Eric OLLIVIER
 """
+__version__ = 0.2
+
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle, Wedge, Polygon
+from matplotlib.patches import Circle, Wedge, Polygon, FancyArrowPatch
 from matplotlib.collections import PatchCollection
 
 import logging as log
+log.basicConfig(level=log.DEBUG)
 
 class MathGraph:
     class ALIGNEMENT:
@@ -19,10 +22,24 @@ class MathGraph:
         DROITE = 'right'
         GAUCHE = 'left'
 
-    def __init__(self, ax=None):
-        self._ax = ax
+    def __init__(self, ax=None, taille=None):
+        """
+        Initialise le graphique.
+        - Si ax n'est pas définit, MathGraph crée automatiquement un nouvel Axes
+          Sinon on utilise celui qui est définit
+        - taille : définit la taille du graphique en pouce. Par défaut (5,5)
+        """
+        if ax is None:
+            if taille is None:
+                taille = (5,5)
+            self._fig, self._ax = plt.subplots(figsize=taille)
+        else:
+            self._ax = ax
+            self._fig = ax.get_figure()
+            if taille is not None:
+                self._fig.set_size_inches(taille)
 
-    def cadre(self, actif=False):
+    def cadre(self, actif=False, taille=None):
         """
         Définit les propriétés du cadre de l'objet Axes
         """
@@ -52,7 +69,8 @@ class MathGraph:
                      ymin=-10, ymax=10,
                      xunit=1, yunit=1,
                      couleur='black',
-                     libelle_origine=None):
+                     libelle_origine=None,
+                     echelle_auto=False):
         """
         Création d'un repère graphique dans un objet de type Axes.
         Paramètre :
@@ -72,14 +90,14 @@ class MathGraph:
 
         # Axe des abscisses
         ax.hlines(y=origine[1], xmin=xmin, xmax=xmax, colors=couleur)
-        for i in range(int(origine[0])-1, int(xmin), -xunit):
+        for i in range(int(origine[0])-1, int(xmin-1), -xunit):
             ax.vlines(x=i,
                       ymin=origine[1]-(ymax-ymin)/200,
                       ymax=origine[1]+(ymax-ymin)/200,
                       colors=couleur)
             ax.annotate(str(i), xy=(i, origine[1]), va='top', ha='center')
 
-        for i in range(int(origine[0])+1, int(xmax), xunit):
+        for i in range(int(origine[0])+1, int(xmax+1), xunit):
             ax.vlines(x=i,
                       ymin=origine[1]-(ymax-ymin)/200,
                       ymax=origine[1]+(ymax-ymin)/200,
@@ -88,14 +106,14 @@ class MathGraph:
 
         # Axe des ordonnées
         ax.vlines(x=origine[0], ymin=ymin, ymax=ymax, colors=couleur)
-        for i in range(int(origine[1])-1, int(ymin), -yunit):
+        for i in range(int(origine[1])-1, int(ymin-1), -yunit):
             ax.hlines(y=i,
                       xmin=origine[0]-(xmax-xmin)/200,
                       xmax=origine[0]+(xmax-xmin)/200,
                       colors=couleur)
             ax.annotate(str(i), xy=(origine[1], i), va='center', ha='right')
 
-        for i in range(int(origine[1])+1, int(ymax), yunit):
+        for i in range(int(origine[1])+1, int(ymax+1), yunit):
             ax.hlines(y=i,
                       xmin=origine[0]-(xmax-xmin)/200,
                       xmax=origine[0]+(xmax-xmin)/200,
@@ -105,6 +123,11 @@ class MathGraph:
         if libelle_origine is None:
             libelle_origine="({},{})".format(*origine)
         ax.annotate(libelle_origine, xy=origine, va='top', ha='right')
+
+        # Traitement de l'échelle
+        if not echelle_auto:
+            self._ax.set_xlim(left=xmin, right=xmax)
+            self._ax.set_ylim(bottom=ymin, top=ymax)
 
         return self
 
@@ -154,7 +177,7 @@ class MathGraph:
         return self
 
     def polygone(self, liste_points=[(0,0), (1,0), (1,1), (0,1)],
-                                              couleur='black'):
+                       couleur='black', style='-'):
         """
         Dessine un polygone à partir d'une liste de points
         """
@@ -164,8 +187,58 @@ class MathGraph:
             return None
 
         ax.add_collection(PatchCollection(
-                [Polygon(liste_points, color=couleur, fill=False)],
+                [Polygon(liste_points, color=couleur, linestyle=style, fill=False)],
                 match_original=True))
 
         return self
 
+    def vecteur(self, origine=(0,0),
+                vecteur=None, destination=None,
+                couleur='black'):
+        """
+        Dessine un vecteur
+        """
+        if vecteur is None and destination is None:
+            vecteur = (1,1);
+        if vecteur is None:
+            vecteur = (destination[0]-origine[0], destination[1]-origine[1])
+
+        arrow_size = 0.02*np.linalg.norm(self._ax.get_xlim())
+        self._ax.arrow(*origine, *vecteur,
+                       head_width=arrow_size, head_length=arrow_size,
+                       color=couleur, length_includes_head=True)
+        return self
+
+
+    def fonction(self, f, xmin=None, xmax=None, nb_points=50, couleur='black'):
+        """
+        Dessine la fonction f entre les bornes [xmin, xmax]
+        avec un échantillon de 'nb_points' sur l'intervalle.
+
+        """
+        # Identification des bornes
+        _xmin, _xmax = self._ax.get_xlim()
+        if xmin is None:
+            xmin = _xmin
+        if xmax is None:
+            xmax = _xmax
+
+        # Calcul de la fonction
+        X = np.linspace(xmin, xmax, nb_points).tolist()
+        Y = []
+        _err = []
+        for i, x in enumerate(X):
+            try:
+                _y = f(x)
+                Y.append(_y)
+            except Exception as err:
+                _err.append(x)
+        for x in _err:
+            X.remove(x)
+
+        # Affichage de la fonction
+        self._ax.plot(X,Y, color=couleur)
+
+        return self
+
+# End of module MathGraph
