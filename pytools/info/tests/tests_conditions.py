3831bedff6acf22dca274224d9fb9f9dd1aa1709
Eric Ollivier <er.ollivier@gmail.com>
2019-05-12 14:17:51 +0200

    Modification du chemin du modules "conditions.py"

--- pytools/info/tests/tests_conditions.py
+++ pytools/info/tests/tests_conditions.py
@@ -6,23 +6,24 @@ Created on Sun Sep 30 11:59:25 2018
 """
 
 import unittest
-from pytools.info.conditions import between, is_iterable
+from prog.conditions.conditions import between, is_iterable
 
 
 class TestBetween(unittest.TestCase):
+
     def test_valeur_hors_bornes(self):
         self.assertFalse(between(0, 0.5, 10))
         self.assertFalse(between(1, -1, 0))
 
     def test_valeur_hors_bornes_exclusif(self):
-        self.assertFalse(between(0,0,1,exclusif=True))
-        self.assertFalse(between(1,0,1,exclusif=True))
+        self.assertFalse(between(0, 0, 1, exclusif=True))
+        self.assertFalse(between(1, 0, 1, exclusif=True))
 
     def test_value_dans_bornes(self):
-        self.assertTrue(between(0.5, -1,1))
+        self.assertTrue(between(0.5, -1, 1))
 
     def test_value_dans_bornes_exclusif(self):
-        self.assertTrue(between(0.5, -1,1, exclusif=True))
+        self.assertTrue(between(0.5, -1, 1, exclusif=True))
 
     def test_iterable_value_all(self):
         self.assertTrue(between([0, 1, 2, 3, 4, 5],
@@ -60,17 +61,19 @@ class TestBetween(unittest.TestCase):
                                 )
                         )
 
+
 class TestIsIterable(unittest.TestCase):
+
     def testIterable(self):
-        self.assertTrue(is_iterable([1,2,3,]))      # list
-        self.assertTrue(is_iterable({1: 'toto'}))   # dict
-        self.assertTrue(is_iterable(range(2)))      # range
-        self.assertTrue(is_iterable([]))            # empty list
-        self.assertTrue(is_iterable("toto"))        # string
+        self.assertTrue(is_iterable([1, 2, 3, ]))  # list
+        self.assertTrue(is_iterable({1: 'toto'}))  # dict
+        self.assertTrue(is_iterable(range(2)))  # range
+        self.assertTrue(is_iterable([]))  # empty list
+        self.assertTrue(is_iterable("toto"))  # string
 
     def testNoIterable(self):
-        self.assertFalse(is_iterable(1))            # number
-        self.assertFalse(is_iterable(True))         # boolean
+        self.assertFalse(is_iterable(1))  # number
+        self.assertFalse(is_iterable(True))  # boolean
 
 
 if __name__ == '__main__':
