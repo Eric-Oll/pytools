1e7203afdd502cd5550493ea689f651c5fcddcee
Eric Ollivier <er.ollivier@gmail.com>
2019-05-12 14:08:19 +0200

    Résolution d'un équation de second degré

--- pytools/math/solving/degree2.py
+++ pytools/math/solving/degree2.py
@@ -6,6 +6,7 @@ Created on Sat May 19 11:52:26 2018
 """
 import numpy as np
 
+
 def solve(a, b, c, precision=1e-7):
     """
     Solve the equation : a.x^2 + b.x + c = 0 with real coeficients
@@ -14,11 +15,11 @@ def solve(a, b, c, precision=1e-7):
     
     Return the list of roots
     """
-    delta = np.square(b)-4*a*c
-    if delta > precision: # 2 real roots
-        return [-b+np.sqrt(delta)/(2*a), -b-np.sqrt(delta)/(2*a)]
-    elif np.abs(delta)<=precision:
-        return [-b/(2*a)] # only one real root
-    else:                 # 2 complex roots
-        return [-b/(2*a)+np.sqrt(-delta)/(2*a)*1j, -b/(2*a)-np.sqrt(-delta)/(2*a)*1j]
+    delta = np.square(b) - 4 * a * c
+    if delta > precision:  # 2 real roots
+        return [-b + np.sqrt(delta) / (2 * a), -b - np.sqrt(delta) / (2 * a)]
+    elif np.abs(delta) <= precision:
+        return [-b / (2 * a)]  # only one real root
+    else:  # 2 complex roots
+        return [-b / (2 * a) + np.sqrt(-delta) / (2 * a) * 1j, -b / (2 * a) - np.sqrt(-delta) / (2 * a) * 1j]
     
