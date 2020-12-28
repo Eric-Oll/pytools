03349da4391136af33e975ec2af1d1407da11e9e
Eric Ollivier <er.ollivier@gmail.com>
2020-12-28 17:38:10 +0100

    EVOL : Ajout de la fonction update_client() pour mettre à jour l'affichage Web à partir du serveur

--- pytools/dataframe/display.py
+++ pytools/dataframe/display.py
@@ -29,6 +29,9 @@ for i in range(20):
 for t, (i, j) in enumerate(zip([int(x) for x in np.random.random_sample(15)*10],
                 [int(x) for x in np.random.random_sample(15)*10])):
     df.iloc[i, j] = [-i, -j, None, f'Exemple texte ({i},{j})'][t%4]
+
+df.iloc[0,1] = pd.to_datetime('2020-12-31')
+df.iloc[1,0] = df.iloc[0,1].date()
 ########################################################################################################################
 
 URL_HEADER = 'data:text/html;charset=utf-8;base64,'
@@ -98,10 +101,23 @@ class Message:
     DATA_SIZE = "size"
     DATA_BUFFER = "data"
 
-    def __init__(self, json_string):
-        self._message = json.loads(json_string)
-        if not isinstance(self._message, dict):
-            raise TypeError(f"{__class__}.__init__ : Bad type for result of json parsing.")
+    def __init__(self, json_string=None, id=None, type=None, title=None, size=None, data=None):
+        if json_string is not None:
+            self._message = json.loads(json_string)
+            if not isinstance(self._message, dict):
+                raise TypeError(f"{__class__}.__init__ : Bad type for result of json parsing.")
+        else:
+            self._message = dict()
+            if id is not None:
+                self[self.ID_CLIENT] = id
+            if type is not None:
+                self[self.TYPE] = type
+            if title is not None:
+                self[self.DATA_TITLE] = title
+            if size is not None:
+                self[self.DATA_SIZE] = size
+            if data is not None:
+                self[self.DATA_BUFFER] = data
 
     def __repr__(self):
         return f"<Message : {str(self)}>"
@@ -172,7 +188,10 @@ class WebDataframeViewer:
         """Arrete la connexion du serveur"""
         cls.Alive_server = False
         cls.Server_connexion.ws_server.close()
-        cls.Client_connexions = dict()
+
+        for client in cls.Client_connexions.copy():
+            cls.Disconnect_client(client)
+#        cls.Client_connexions = dict()
         cls.Instances = dict()
 
     @classmethod
@@ -182,6 +201,7 @@ class WebDataframeViewer:
             client.close()
         except:
             pass
+        cls.Instances[cls.Client_connexions[client]].connexion = None
         del cls.Client_connexions[client]
         if len(cls.Client_connexions) == 0:
             cls.Stop_server()
@@ -189,17 +209,21 @@ class WebDataframeViewer:
     @classmethod
     async def server_daemon(cls, client, path):
 
+        log.debug(f"Client={client}, path={path}")
         while cls.Alive_server:
             try:
-                async for message_buffer in client:
-                    message = Message(message_buffer)
+                # Réception des messages
+                async for message_buffer_in in client:
+                    message = Message(message_buffer_in)
                     log.debug(f"{__class__}.server_daemon : Message reçu : " + repr(message))
                     try:
                         if message.message_type == MESSAGE_TYPE.CONNECTION:
                             log.debug(f"{__class__}.server_daemon : Nouvelle connexion {client} - Client " + str(message.id_client))
                             cls.Client_connexions[client] = message.id_client
+                            cls.Instances[message.id_client].connexion = client
                             message.message_type = MESSAGE_TYPE.ACKNOWLEDGE
                             await client.send(str(message))
+                            #cls.Instances[message.id_client].add_output_message(str(message))
 
                         elif message.message_type == MESSAGE_TYPE.DISCONNECTION:
                             cls.Disconnect_client(client)
@@ -215,6 +239,8 @@ class WebDataframeViewer:
                                 message[Message.DATA_TITLE] = instance.title
                                 message[Message.DATA_SIZE] = instance.size
                                 await client.send(str(message))
+
+                                #instance.add_output_message(str(instance.get_message()))
                             else: # L'instance n'existe pas => On déconnecte
                                 try:
                                     cls.Disconnect_client(client)
@@ -225,6 +251,15 @@ class WebDataframeViewer:
                             raise Exception(f"{__class__}.server_daemon : Type de message inconnu. (type de message {type(message)})")
                     except Exception as err:
                         log.error(f"{__class__}.server_daemon :  : Format de message incorrect : {type(message)}{message}")
+
+                    # Envoie des messages
+                    # instance = cls.Instances[cls.Client_connexions[client]]
+                    # log.debug(f"{__class__}.server_daemon :  : Nb Output Messages={instance.nb_output_messages}")
+                    # while instance.nb_output_messages > 0:
+                    #     output_message = instance.next_output_message()
+                    #     #log.debug(f"{__class__}.server_daemon : OUTPUT_MESSAGE => {output_message}")
+                    #     await client.send(output_message)
+
             except websockets.exceptions.ConnectionClosedError as err:
                 log.debug(f"{__class__}.server_daemon :  : Disconnection from {client}\n-> {err}")
                 cls.Disconnect_client(client)
@@ -295,7 +330,9 @@ class WebDataframeViewer:
         self._browser = browser_executable
         self._title = title
         self._id_client = self.get_IdClient()
+        self._connexion = None # Objet de connexion client
         self.Instances[self.id_client] = self
+        self._output_messages_buffer = []
 
         # Contrôle l'état du serveur. Si STOP alors le lancer
         if not self.Alive_server:
@@ -321,13 +358,46 @@ class WebDataframeViewer:
     def title(self):
         return self._title
 
+    @title.setter
+    def title(self, new_title):
+        self._title = new_title
+        self.update_client()
+
     @property
     def dataframe(self):
         return self._dataframe
 
+    @dataframe.setter
+    def dataframe(self, new_dataframe):
+        self._dataframe = new_dataframe
+        self.update_client()
+
     @property
     def size(self):
         return len(self.dataframe)
+
+    @property
+    def connexion(self):
+        return self._connexion
+
+    @connexion.setter
+    def connexion(self, value):
+        self._connexion = value
+
+    @property
+    def nb_output_messages(self):
+        return len(self._output_messages_buffer)
+
+    def add_output_message(self, message):
+        self._output_messages_buffer.append(message)
+
+    def next_output_message(self):
+        if self.nb_output_messages > 0:
+            message = self._output_messages_buffer.pop(0)
+            log.debug(f"{__class__}.next_output_message : Message={message}")
+            return message
+        else:
+            return None
 
     def dataframe_to_html(self):
         buffer = self.dataframe.fillna("<null>").applymap(lambda x: str(x) if isinstance(x, (datetime.datetime, datetime.date)) else x).to_dict()
@@ -346,10 +416,23 @@ class WebDataframeViewer:
             .replace("{id_client}", str(self.id_client))
 
         # Lancement du browser et affichage de la page
-        log.debug(f"{__class__}.create_html_client : Page HTML\n{html}")
+        #log.debug(f"{__class__}.create_html_client : Page HTML\n{html}")
         subprocess.call([self.browser, URL_HEADER + str(base64.b64encode(bytes(html, 'utf-8')))[2:-1]])
 
+    def get_message(self):
+        return Message(id=self.id_client,
+                       type=MESSAGE_TYPE.DATA_REFRESH,
+                       title=self.title,
+                       size=self.size,
+                       data=self.dataframe_to_html()
+                       )
 
+    def update_client(self):
+        """
+        Envoie une mise à jour du dataframe au client
+        :return:
+        """
+        asyncio.run(self.connexion.send(str(self.get_message())))
 
 
 
