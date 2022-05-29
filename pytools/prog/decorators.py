371960dd91587ffa69b3aeb6b5755b610b05b97f
Eric Ollivier <er.ollivier@gmail.com>
2022-05-29 11:45:03 +0200

    ADD : Create deprecated function for mark an old function

--- pytools/prog/decorators.py
+++ pytools/prog/decorators.py
@@ -5,7 +5,22 @@ Created on Sun May 13 13:55:16 2018
 @author: Eric
 """
 import time
+import warnings
+import functools
 
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+    return new_func
 
 def timemark(label=''):
 
